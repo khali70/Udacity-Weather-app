bf0b8f05538263d8c18a08fc8c39923238ad7ad1
khali70 <k7alifa.a7med70@gmail.com>
2021-01-08 14:46:16 +0200

    update the UI 1

--- website/app.js
+++ website/app.js
@@ -1,5 +1,6 @@
 /* Global Variables */
-
+// apiKey 7e161dfccb949ec489a187df12c70cd7
+// api.openweathermap.org/data/2.5/weather?q={city name},{state code},{country code}&appid={API key}
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
